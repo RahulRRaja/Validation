dfe63c75b04141a1d37a777f4b51b0831dc6313e
RahulRRaja <93483148+RahulRRaja@users.noreply.github.com>
2021-11-12 21:01:05 +0530

    Add files via upload

---
+++ validation/src/main/java/com/sample/validation/repository/DetailsRepository.java
@@ -0,0 +1,7 @@
+package com.sample.validation.repository;
+
+import com.sample.validation.model.Details;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DetailsRepository extends CrudRepository<Details,Long> {
+}
